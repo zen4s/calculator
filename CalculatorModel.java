6daeaec9d380fae0f25685c002fdd68104f4b33e
zen4s <zen4s@users.noreply.github.com>
2017-04-16 09:58:11 -0700

    Update CalculatorModel.java

--- CalculatorModel.java
+++ CalculatorModel.java
@@ -4,20 +4,16 @@
 
 public class CalculatorModel {
 
-	// Holds the value of the sum of the numbers
+	// Holds it the value of the sum of the numbers
 	// entered in the view
 	
 	private int calculationValue;
 	
-	public void addTwoNumbers(int firstNumber, int secondNumber){
-		
-		calculationValue = firstNumber + secondNumber;
-		
+	public void addTwoNumbers(int firstNumber, int secondNumber){		
+		calculationValue = firstNumber + secondNumber;		
 	}
 	
-	public int getCalculationValue(){
-		
+	public int getCalculationValue(){	
 		return calculationValue;
-		
 	}
 }
